ec842f40699c84d61a2396f197233d3f1c47c35d
James Barnett <james.barnett@fivium.co.uk>
2019-03-26 15:15:17 +0000

    Add healthcheck endpoint. Log render durations

--- src/main/java/uk/co/fivium/els/renderer/JpegRenderer.java
+++ src/main/java/uk/co/fivium/els/renderer/JpegRenderer.java
@@ -1,11 +1,15 @@
 package uk.co.fivium.els.renderer;
 
+import com.google.common.base.Stopwatch;
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.TimeUnit;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.JPEGTranscoder;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -14,9 +18,12 @@ import uk.co.fivium.els.util.TemplateUtils;
 @Service
 public class JpegRenderer implements Renderer {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(JpegRenderer.class);
+
   @Override
   public Resource render(Document svg) {
     try {
+      Stopwatch renderStopwatch = Stopwatch.createStarted();
       TranscoderInput transcoderInput = new TranscoderInput(IOUtils.toInputStream(TemplateUtils.getSvgElement(svg).outerHtml(), "UTF-8"));
 
       ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -24,6 +31,8 @@ public class JpegRenderer implements Renderer {
       JPEGTranscoder jpegTranscoder= new JPEGTranscoder();
       jpegTranscoder.addTranscodingHint(JPEGTranscoder.KEY_QUALITY, 1f);
       jpegTranscoder.transcode(transcoderInput, transcoderOutput);
+
+      LOGGER.info("JPEG internet label generated. Took [{}ms]", renderStopwatch.elapsed(TimeUnit.MILLISECONDS));
 
       return new ByteArrayResource(os.toByteArray());
 
--- src/main/java/uk/co/fivium/els/renderer/PngRenderer.java
+++ src/main/java/uk/co/fivium/els/renderer/PngRenderer.java
@@ -1,11 +1,15 @@
 package uk.co.fivium.els.renderer;
 
+import com.google.common.base.Stopwatch;
 import java.io.ByteArrayOutputStream;
+import java.util.concurrent.TimeUnit;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.apache.commons.io.IOUtils;
 import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -14,16 +18,21 @@ import uk.co.fivium.els.util.TemplateUtils;
 @Service
 public class PngRenderer implements Renderer {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(PngRenderer.class);
+
   @Override
   public Resource render(Document svg) {
 
     try {
+      Stopwatch renderStopwatch = Stopwatch.createStarted();
       TranscoderInput transcoderInput = new TranscoderInput(IOUtils.toInputStream(TemplateUtils.getSvgElement(svg).outerHtml(), "UTF-8"));
 
       ByteArrayOutputStream os = new ByteArrayOutputStream();
       TranscoderOutput transcoderOutput = new TranscoderOutput(os);
       PNGTranscoder pngTranscoder = new PNGTranscoder();
       pngTranscoder.transcode(transcoderInput, transcoderOutput);
+
+      LOGGER.info("PNG internet label generated. Took [{}ms]", renderStopwatch.elapsed(TimeUnit.MILLISECONDS));
 
       return new ByteArrayResource(os.toByteArray());
 
