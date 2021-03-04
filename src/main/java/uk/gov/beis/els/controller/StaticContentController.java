fb8c81baa3f47aa576af13e83f1489119391d085
Matt Eason <matt.eason@fivium.co.uk>
2021-03-04 14:23:37 +0000

    Check referer is from a valid domain before using on cookies page

--- src/main/java/uk/gov/beis/els/controller/StaticContentController.java
+++ src/main/java/uk/gov/beis/els/controller/StaticContentController.java
@@ -1,5 +1,6 @@
 package uk.gov.beis.els.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,9 +15,16 @@ public class StaticContentController {
   }
 
   @GetMapping("/cookies")
-  public ModelAndView renderCookiePolicy(@RequestHeader(value = "referer", required = false) final String referer) {
+  public ModelAndView renderCookiePolicy(
+      @RequestHeader(value = "referer", required = false) final String referer,
+      @Value("${app.valid_referer_domain}") String validRefererDomain
+  ) {
     ModelAndView modelAndView = new ModelAndView("staticContent/cookiePolicy");
-    modelAndView.addObject("referer", referer);
+    if(referer != null && referer.startsWith(validRefererDomain)) {
+      modelAndView.addObject("referer", referer);
+    } else {
+      modelAndView.addObject("referer", null);
+    }
     return modelAndView;
   }
 
