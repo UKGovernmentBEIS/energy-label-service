e7e904c9ceadb5c9511627f09459797e9ba809e1
framirez <fernando.ramirez@fivium.co.uk>
2022-02-02 12:13:01 +0000

    Remove old style labels for Televisions and electronic displays Updated template, form, service, controller and sequence provider

--- src/main/java/uk/gov/beis/els/categories/televisions/model/validation/TelevisionsFormSequenceProvider.java
+++ src/main/java/uk/gov/beis/els/categories/televisions/model/validation/TelevisionsFormSequenceProvider.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang3.BooleanUtils;
 import org.hibernate.validator.spi.group.DefaultGroupSequenceProvider;
-import uk.gov.beis.els.categories.common.PostMarch2021Field;
-import uk.gov.beis.els.categories.common.PreMarch2021Field;
 import uk.gov.beis.els.categories.televisions.model.TelevisionsForm;
-import uk.gov.beis.els.categories.televisions.service.TelevisionsService;
 
 public class TelevisionsFormSequenceProvider implements DefaultGroupSequenceProvider<TelevisionsForm> {
   @Override
@@ -15,13 +12,8 @@ public class TelevisionsFormSequenceProvider implements DefaultGroupSequenceProv
     List<Class<?>> sequence = new ArrayList<>();
 
     if (form != null) {
-      if (TelevisionsService.LEGISLATION_CATEGORY_PRE_MARCH_2021.getId().equals(form.getApplicableLegislation())) {
-        sequence.add(PreMarch2021Field.class);
-      } else if (TelevisionsService.LEGISLATION_CATEGORY_POST_MARCH_2021.getId().equals(form.getApplicableLegislation())) {
-        sequence.add(PostMarch2021Field.class);
-        if(BooleanUtils.isTrue(form.getIsHdr())) {
-          sequence.add(HdrGroup.class);
-        }
+      if(BooleanUtils.isTrue(form.getIsHdr())) {
+        sequence.add(HdrGroup.class);
       }
     }
     sequence.add(TelevisionsForm.class);
