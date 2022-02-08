b026a30be3fab62ceaef6741e18969b3c888e07d
framirez <fernando.ramirez@fivium.co.uk>
2022-02-08 16:31:51 +0000

    ELG-12 API for local space heaters

---
+++ src/main/java/uk/gov/beis/els/api/categories/localspaceheaters/LocalSpaceHeatersInternetLabelApiForm.java
@@ -0,0 +1,24 @@
+package uk.gov.beis.els.api.categories.localspaceheaters;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import uk.gov.beis.els.api.common.ApiValuesFromLegislationCategory;
+import uk.gov.beis.els.api.common.BaseInternetLabelApiForm;
+import uk.gov.beis.els.categories.localspaceheaters.service.LocalSpaceHeatersService;
+
+@Schema(name = "Local space heaters internet label")
+public class LocalSpaceHeatersInternetLabelApiForm extends BaseInternetLabelApiForm {
+
+  @Schema(description = "The energy efficiency class of the model")
+  @NotBlank
+  @ApiValuesFromLegislationCategory(serviceClass = LocalSpaceHeatersService.class)
+  private String efficiencyRating;
+
+  public String getEfficiencyRating() {
+    return efficiencyRating;
+  }
+
+  public void setEfficiencyRating(String efficiencyRating) {
+    this.efficiencyRating = efficiencyRating;
+  }
+}
