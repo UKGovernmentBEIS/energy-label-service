4d750be200a17bfd16044e12ccfddeba6cb1bb23
dbetts <danny.betts@fivium.co.uk>
2022-02-10 16:08:05 +0000

    Add solid fuel boiler API endpoints and order the schemas, tags, and operations in the OpenApi document

--- src/main/java/uk/gov/beis/els/api/openapi/OpenApiConfig.java
+++ src/main/java/uk/gov/beis/els/api/openapi/OpenApiConfig.java
@@ -2,10 +2,15 @@ package uk.gov.beis.els.api.openapi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import java.util.Map;
+import java.util.TreeMap;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SuppressWarnings("rawtypes")
 public class OpenApiConfig {
 
   @Bean
@@ -16,6 +21,13 @@ public class OpenApiConfig {
           .title("Create an energy label")
           .description("Create a label for an energy-related product (ErP) that you make or sell to UK customers, for example white goods, boilers and other household appliances.")
       );
+  }
+  @Bean
+  public OpenApiCustomiser sortSchemasAlphabetically() {
+    return openApi -> {
+      Map<String, Schema> schemas = openApi.getComponents().getSchemas();
+      openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+    };
   }
 
 }
