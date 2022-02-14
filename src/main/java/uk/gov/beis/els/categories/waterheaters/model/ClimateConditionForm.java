224d0b5d64ec33a04b9bacf2b9adbbba5731c378
dbetts <danny.betts@fivium.co.uk>
2022-02-14 14:35:23 +0000

    Add water heater API endpoints

--- src/main/java/uk/gov/beis/els/categories/waterheaters/model/ClimateConditionForm.java
+++ src/main/java/uk/gov/beis/els/categories/waterheaters/model/ClimateConditionForm.java
@@ -1,7 +1,10 @@
 package uk.gov.beis.els.categories.waterheaters.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import uk.gov.beis.els.api.common.ApiValuesFromEnum;
 import uk.gov.beis.els.categories.common.StandardTemplateForm30Char;
 import uk.gov.beis.els.categories.waterheaters.model.validation.ConsumptionUnitBoth;
 import uk.gov.beis.els.categories.waterheaters.model.validation.ConsumptionUnitGj;
@@ -12,56 +15,81 @@ public class ClimateConditionForm extends StandardTemplateForm30Char {
 
   @FieldPrompt("How do you want to provide annual energy consumption?")
   @NotBlank(message = "Specify how you want to provide annual energy consumption")
+  @ApiValuesFromEnum(EnergyConsumptionUnit.class)
   private String consumptionUnit;
 
   @FieldPrompt("Colder climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for colder climate conditions, up to 4 digits long", groups = ConsumptionUnitKw.class)
+  @NotNull(groups = ConsumptionUnitKw.class)
+  @Schema(type = "integer")
   private String colderKwhAnnumSingle;
 
   @FieldPrompt("Average climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for average climate conditions, up to 4 digits long", groups = ConsumptionUnitKw.class)
+  @NotNull(groups = ConsumptionUnitKw.class)
+  @Schema(type = "integer")
   private String averageKwhAnnumSingle;
 
   @FieldPrompt("Warmer climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for warmer climate conditions, up to 4 digits long", groups = ConsumptionUnitKw.class)
+  @NotNull(groups = ConsumptionUnitKw.class)
+  @Schema(type = "integer")
   private String warmerKwhAnnumSingle;
 
   @FieldPrompt("Colder climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for colder climate conditions, up to 2 digits long", groups = ConsumptionUnitGj.class)
+  @NotNull(groups = ConsumptionUnitGj.class)
+  @Schema(type = "integer")
   private String colderGjAnnumSingle;
 
   @FieldPrompt("Average climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for average climate conditions, up to 2 digits long", groups = ConsumptionUnitGj.class)
+  @NotNull(groups = ConsumptionUnitGj.class)
+  @Schema(type = "integer")
   private String averageGjAnnumSingle;
 
   @FieldPrompt("Warmer climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for warmer climate conditions, up to 2 digits long", groups = ConsumptionUnitGj.class)
+  @NotNull(groups = ConsumptionUnitGj.class)
+  @Schema(type = "integer")
   private String warmerGjAnnumSingle;
 
 
   // Dual unit fields must be separate to avoid them being treated as lists.
   @FieldPrompt("Colder climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for colder climate conditions, up to 4 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String colderKwhAnnumBoth;
 
   @FieldPrompt("Average climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for average climate conditions, up to 4 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String averageKwhAnnumBoth;
 
   @FieldPrompt("Warmer climate conditions")
   @Digits(integer = 4, fraction = 0, message = "Enter the annual electricity consumption for warmer climate conditions, up to 4 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String warmerKwhAnnumBoth;
 
   @FieldPrompt("Colder climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for colder climate conditions, up to 2 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String colderGjAnnumBoth;
 
   @FieldPrompt("Average climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for average climate conditions, up to 2 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String averageGjAnnumBoth;
 
   @FieldPrompt("Warmer climate conditions")
   @Digits(integer = 2, fraction = 0, message = "Enter the annual fuel consumption for warmer climate conditions, up to 2 digits long", groups = ConsumptionUnitBoth.class)
+  @NotNull(groups = ConsumptionUnitBoth.class)
+  @Schema(type = "integer")
   private String warmerGjAnnumBoth;
 
 
--- src/main/java/uk/gov/beis/els/categories/waterheaters/model/HeatPumpWaterHeatersForm.java
+++ src/main/java/uk/gov/beis/els/categories/waterheaters/model/HeatPumpWaterHeatersForm.java
@@ -1,28 +1,36 @@
 package uk.gov.beis.els.categories.waterheaters.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.groups.Default;
 import org.hibernate.validator.group.GroupSequenceProvider;
+import uk.gov.beis.els.api.common.ApiValuesFromEnum;
+import uk.gov.beis.els.api.common.ApiValuesFromLegislationCategory;
+import uk.gov.beis.els.categories.common.LoadProfile;
 import uk.gov.beis.els.categories.internetlabelling.model.InternetLabellingGroup;
 import uk.gov.beis.els.categories.waterheaters.model.validation.HeatPumpWaterHeatersFormSequenceProvider;
+import uk.gov.beis.els.categories.waterheaters.service.WaterHeatersService;
 import uk.gov.beis.els.model.meta.DualModeField;
 import uk.gov.beis.els.model.meta.FieldPrompt;
 import uk.gov.beis.els.model.meta.StaticProductText;
 
+@Schema(name = "Heat pump water heater energy label")
 @StaticProductText("You must display the label at the point of sale so that it’s easy to see and clearly related to the product. It must be at least 105mm x 200mm when printed.")
 @GroupSequenceProvider(HeatPumpWaterHeatersFormSequenceProvider.class)
 public class HeatPumpWaterHeatersForm extends ClimateConditionForm {
 
   @FieldPrompt("Declared load profile")
   @NotBlank(message = "Select a declared load profile")
+  @ApiValuesFromEnum(value = LoadProfile.class)
   private String declaredLoadProfile;
 
   @FieldPrompt("Water heating energy efficiency class")
   @NotBlank(message = "Select an energy efficiency indicator", groups = {Default.class, InternetLabellingGroup.class})
   @DualModeField
+  @ApiValuesFromLegislationCategory(serviceClass = WaterHeatersService.class)
   private String efficiencyRating;
 
   // TODO Create Digits variant which allows optional. Auto set (optional) in prompt
@@ -32,6 +40,8 @@ public class HeatPumpWaterHeatersForm extends ClimateConditionForm {
 
   @FieldPrompt("Sound power level, outdoors dB")
   @Digits(integer = 2, fraction = 0, message = "Enter the outdoors sound power level, up to 2 digits long")
+  @NotNull
+  @Schema(type = "integer")
   private String soundPowerLevelOutdoors;
 
   @FieldPrompt("Can the heat pump be set to work only during off-peak hours?")
