9bd9a03e91002263d98d5743d0ff38625580c290
dannybetts <98107825+dannybetts@users.noreply.github.com>
2022-02-14 10:01:24 +0000

    Add solid fuel boiler API endpoints and order the schemas, tags, and operations in the OpenApi document (#97)

---
+++ src/main/java/uk/gov/beis/els/api/categories/solidfuelboilers/SolidFuelBoilersInternetLabelApiForm.java
@@ -0,0 +1,24 @@
+package uk.gov.beis.els.api.categories.solidfuelboilers;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotNull;
+import uk.gov.beis.els.api.common.ApiValuesFromLegislationCategory;
+import uk.gov.beis.els.api.common.BaseInternetLabelApiForm;
+import uk.gov.beis.els.categories.solidfuelboilers.service.SolidFuelBoilersService;
+
+@Schema(name = "Solid fuel boiler arrow image")
+public class SolidFuelBoilersInternetLabelApiForm extends BaseInternetLabelApiForm {
+
+  @Schema(description = "The energy efficiency class indicator")
+  @NotNull
+  @ApiValuesFromLegislationCategory(serviceClass = SolidFuelBoilersService.class)
+  private String efficiencyRating;
+
+  public String getEfficiencyRating() {
+    return efficiencyRating;
+  }
+
+  public void setEfficiencyRating(String efficiencyRating) {
+    this.efficiencyRating = efficiencyRating;
+  }
+}
