03d766515f01fad933e44f860249c62dbd48b1ff
framirez <fernando.ramirez@fivium.co.uk>
2022-02-18 16:39:49 +0000

    FreezerStarRating implements displayable

--- src/main/java/uk/gov/beis/els/categories/refrigeratingappliances/model/FreezerStarRating.java
+++ src/main/java/uk/gov/beis/els/categories/refrigeratingappliances/model/FreezerStarRating.java
@@ -1,11 +1,17 @@
 package uk.gov.beis.els.categories.refrigeratingappliances.model;
 
-public enum FreezerStarRating {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import uk.gov.beis.els.model.Displayable;
+
+public enum FreezerStarRating implements Displayable {
   ONE_STAR("One star", "starRating1"),
   TWO_STAR("Two star", "starRating2"),
   THREE_STAR("Three star", "starRating3"),
   FOUR_STAR("Four star", "starRating4");
 
+  private static final Map<String, FreezerStarRating> ENUM_MAP;
   private final String displayName;
   private final String templateStarRatingClassName;
 
@@ -14,11 +20,28 @@ public enum FreezerStarRating {
     this.templateStarRatingClassName = templateStarRatingClassName;
   }
 
+  @Override
   public String getDisplayName() {
     return displayName;
   }
 
   public String getTemplateStarRatingClassName() {
     return templateStarRatingClassName;
+  }
+
+  static {
+    Map<String, FreezerStarRating> map = new HashMap<String, FreezerStarRating>();
+    for (FreezerStarRating instance : FreezerStarRating.values()) {
+      map.put(instance.getDisplayName().toLowerCase(), instance);
+    }
+    ENUM_MAP = Collections.unmodifiableMap(map);
+  }
+
+  public static FreezerStarRating getEnum(String freezerStarRating) {
+    FreezerStarRating result = ENUM_MAP.get(freezerStarRating.toLowerCase());
+    if (result == null) {
+      result = FreezerStarRating.valueOf(freezerStarRating);
+    }
+    return result;
   }
 }
