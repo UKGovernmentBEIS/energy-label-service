70703a416e6d42b57e68ad5c998f69a5a27fe08e
James Barnett <james.barnett@fivium.co.uk>
2022-02-21 16:33:01 +0000

    Fix stub test

--- src/test/java/uk/gov/beis/els/apiintegration/RateLimitingIntegrationTest.java
+++ src/test/java/uk/gov/beis/els/apiintegration/RateLimitingIntegrationTest.java
@@ -1,28 +1,30 @@
 package uk.gov.beis.els.apiintegration;
 
-import org.junit.Before;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import uk.gov.beis.els.service.Bucket4JRequestService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(value = RateLimitingIntegrationTest.TestConfig.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ActiveProfiles("bucket4j-integration-test")
+@AutoConfigureMockMvc
 public class RateLimitingIntegrationTest {
 
+  @Autowired
   private MockMvc mockMvc;
 
   @Autowired
@@ -31,41 +33,28 @@ public class RateLimitingIntegrationTest {
   @Autowired
   protected Bucket4JRequestService bucket4JRequestService;
 
-  @Before
-  public void setUp() throws Exception {
-    mockMvc = MockMvcBuilders
-        .webAppContextSetup(context)
-        .build();
-  }
-
-  //TODO fix integration test: raised ELG-39 for this
   @Test
   public void withSingleIpAddress() throws Exception {
 
-//    String apiContent = "{\n" +
-//        "  \"supplierName\": \"string\",\n" +
-//        "  \"modelName\": \"string\",\n" +
-//        "  \"efficiencyRating\": \"APPP\",\n" +
-//        "  \"volume\": 0,\n" +
-//        "  \"conventionalKwhConsumption\": 0,\n" +
-//        "  \"isFanOven\": true,\n" +
-//        "  \"convectionKwhConsumption\": 0,\n" +
-//        "  \"conventionalMjConsumption\": 0,\n" +
-//        "  \"convectionMjConsumption\": 0\n" +
-//        "}";
-//
-//    mockMvc.perform(post("/api/v1/domestic-ovens/gas-ovens/energy-label")
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .content(apiContent)
-//            .accept(MediaType.APPLICATION_JSON)
-//            .header("X-Forwarded-For", "1.1.1.1"))
-//        .andExpect(status().isOk())
-//        .andExpect(header().longValue("X-Rate-Limit-Remaining", 2));
+    String apiContent = "{\n" +
+        "  \"supplierName\": \"string\",\n" +
+        "  \"modelName\": \"string\",\n" +
+        "  \"efficiencyRating\": \"APPP\",\n" +
+        "  \"volume\": 0,\n" +
+        "  \"conventionalKwhConsumption\": 0,\n" +
+        "  \"isFanOven\": true,\n" +
+        "  \"convectionKwhConsumption\": 0,\n" +
+        "  \"conventionalMjConsumption\": 0,\n" +
+        "  \"convectionMjConsumption\": 0\n" +
+        "}";
+
+    mockMvc.perform(post("/api/v1/domestic-ovens/gas-ovens/energy-label")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(apiContent)
+            .accept(MediaType.APPLICATION_JSON)
+            .header("X-Forwarded-For", "1.1.1.1"))
+        .andExpect(status().isOk())
+        .andExpect(header().longValue("X-Rate-Limit-Remaining", 2));
   }
 
-  @TestConfiguration
-  @EnableCaching
-  static class TestConfig {
-
-  }
 }
