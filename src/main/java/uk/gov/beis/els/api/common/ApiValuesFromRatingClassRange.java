4092e09ec4642e07930eaf46478180debbf42c5f
FernandoRamirez-De-Aguilar <98316465+FernandoRamirez-De-Aguilar@users.noreply.github.com>
2022-02-23 15:48:47 +0000

    Feature/ELG-35/validate rating ranges passed to API endpoints (#112)
    * ELG-38 validate ratingRanges and Enum values passed to API
    added tests

--- src/main/java/uk/gov/beis/els/api/common/ApiValuesFromRatingClassRange.java
+++ src/main/java/uk/gov/beis/els/api/common/ApiValuesFromRatingClassRange.java
@@ -5,6 +5,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
 
 /**
  * Fields with this annotation will have their allowableValues constraint in the OpenAPI specification set dynamically based on
@@ -12,9 +14,14 @@ import java.lang.annotation.Target;
  *
  * This will override any static allowableValues set in a @ApiModelProperty annotation.
  */
+@Constraint(validatedBy = ApiValuesFromRatingClassRangeValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ApiValuesFromRatingClassRange {
+  String message() default "{uk.gov.beis.els.api.common.ApiValuesFromRatingClassRange.message}";
+  Class<?>[] groups() default {};
+  Class<? extends Payload>[] payload() default {};
+
 
   /**
    * @return The class which contains the RatingClassRange field
